de7f6b9c5e90a65e0f779830613e05a23e5d3904
Tej Pochiraju <tej@iotready.co>
2023-01-09 17:59:35 +0530

    Add separate settings page

---
+++ iotready_warehouse_traceability_frappe/iotready_warehouse_traceability/doctype/iotready_traceability_settings/iotready_traceability_settings.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, IoTReady and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('IoTReady Traceability Settings', {
+	// refresh: function(frm) {
+
+	// }
+});
