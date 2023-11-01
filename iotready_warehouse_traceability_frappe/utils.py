58915ad2fbb45204c613bb2302b412c99b6b3f18
Tej Pochiraju <tej@iotready.co>
2023-11-01 23:44:02 +0530

    Add log_ingress

--- iotready_warehouse_traceability_frappe/utils.py
+++ iotready_warehouse_traceability_frappe/utils.py
@@ -1,7 +1,15 @@
 import json
 import frappe
-from datetime import datetime, timedelta
+from datetime import date, datetime, timedelta
 from iotready_warehouse_traceability_frappe.validations import *
+
+
+def date_json_serial(obj):
+    """JSON serializer for objects not serializable by default json code"""
+
+    if isinstance(obj, (datetime, date)):
+        return obj.isoformat()
+    raise TypeError("Type %s not serializable" % type(obj))
 
 
 def get_user_warehouse_hook():
--- iotready_warehouse_traceability_frappe/workflows.py
+++ iotready_warehouse_traceability_frappe/workflows.py
@@ -1,6 +1,7 @@
 import frappe
 import json
-from iotready_warehouse_traceability_frappe import utils, db
+from datetime import datetime, timedelta
+from iotready_warehouse_traceability_frappe import db
 
 SCAN_CHAR = "d3edcb21-d7aa-4731-bad8-946651538501"
 WEIGHT_CHAR = "d3edcb21-d7aa-4731-bad8-946651538502"
@@ -30,7 +31,7 @@ def get_activity_session(session_id: str):
     return context
 
 
-def update_activity_session(session_id: str, new_context: dict):
+def update_activity_session(session_id: str, new_context: dict | str):
     if not new_context:
         return None
     context = get_activity_session(session_id)
@@ -44,3 +45,25 @@ def update_activity_session(session_id: str, new_context: dict):
         f"activity_session:{session_id}", frappe.as_json(context), ex=86400
     )
     return context
+
+def log_ingress(crates, activity, result, creation=None):
+    try:
+        log = frappe.new_doc("Ingress Log")
+        log.name = frappe.generate_hash(length=10)
+        if creation:
+            log.creation = creation
+        else:
+            log.creation = datetime.now() + timedelta(hours=5, minutes=30)
+        log.modified = datetime.now() + timedelta(hours=5, minutes=30)
+        log.owner = frappe.session.user
+        log.modified_by = frappe.session.user
+        log.activity = activity
+        # log.crate = json.dumps(crate)
+        log.result = json.dumps(result)
+        if len(crates) > 0:
+            log.crate_id = crates[0].get("crate_id")
+        log.raw_payload = json.dumps({"crates": crates, "activity": activity})
+        db.deferred_insert(log)
+    except Exception as e:
+        print("Log Ingress", str(e))
+
